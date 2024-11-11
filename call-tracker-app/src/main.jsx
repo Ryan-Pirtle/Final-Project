ff233b9ed4a97fa7b4cbb714822c201fee1198cb
Ryan Pirtle <65506755+Ryan-Pirtle@users.noreply.github.com>
2024-11-10 20:31:37 -0600

    imported css file

--- call-tracker-app/src/main.jsx
+++ call-tracker-app/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import LoginPage from './LoginPage.jsx'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
